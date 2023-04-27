f4851b0fca38fcdb18e3481b0293875541e5da2e
Mercedes Flowers <mercedes.flowers@kellogg.northwestern.edu>
2023-04-27 02:30:24 +0000

    More labs

--- labs/3-associations.rb
+++ labs/3-associations.rb
@@ -17,8 +17,62 @@ Activity.destroy_all
 # 1. insert 3 rows in the activities table with relationships to
 # a single salesperson and 2 different contacts
 
+new_activity = Activity.new
+
+puts new_activity.inspect
+
+salesguy = Salesperson.find_by({"first_name" => "Abe"})
+jeff = Contact.find_by({"first_name" => "Jeff"})
+
+new_activity["created"] = "4/26/23"
+new_activity["notes"] = "bowling with amazon"
+new_activity["salesperson_id"] = salesguy["id"]
+new_activity["contact_id"] = jeff["id"]
+new_activity.save # inserts into the table
+
+puts new_activity.inspect
+
+activity_2 = Activity.new
+
+puts activity_2.inspect
+
+craig = Contact.find_by({"first_name" => "Craig"})
+
+activity_2["created"] = "4/25/23"
+activity_2["notes"] = "kbbq with apple"
+activity_2["salesperson_id"] = salesguy["id"]
+activity_2["contact_id"] = craig["id"]
+activity_2.save # inserts into the table
+
+puts activity_2.inspect
+
+activity_3 = Activity.new
+
+puts activity_3.inspect
+
+tim = Contact.find_by({"first_name" => "Tim"})
+
+activity_3["created"] = "4/23/23"
+activity_3["notes"] = "skating with apple"
+activity_3["salesperson_id"] = salesguy["id"]
+activity_3["contact_id"] = tim["id"]
+activity_3.save # inserts into the table
+
+puts activity_3.inspect
+
+
 # 2. Display all the activities between the salesperson used above
 # and one of the contacts (sample output below):
+
+relevant_activity = Activity.where({"salesperson_id" => salesguy["id"], "contact_id" => tim["id"]})
+
+puts relevant_activity.inspect
+
+puts "Activities between ..."
+for activity in relevant_activity
+puts activity["notes"]
+
+end
 
 # ---------------------------------
 # Activities between Ben and Tim Cook:
